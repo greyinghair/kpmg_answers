930b1bbd47f61d68105c797b990a362a3230093f
greyinghair <dan@stacey.im>
2020-05-03 17:55:38 +0000

    lession 3 section B questions completed

--- lesson_3/questions_b.py
+++ lesson_3/questions_b.py
@@ -89,7 +89,26 @@ else:
 ## Question 6 ##
 print("###Question 6###\n")
 # Take the age and name of three people and determine who is the oldest and youngest and print out the name and age of the oldest and youngest. If all three ages are the same, print that.
-
+name1 = input("What is person #1's name? ")
+age1 = int(input("What is the age of " + name1 + "? "))
+name2 = input("What is person #2's name? ")
+age2 = int(input("What is the age of " + name2 + "? "))
+name3 = input("What is person #3's name? ")
+age3 = int(input("What is the age of " + name3 + "? "))
+if age1 == age2 and age2 == age3:
+    print(name1 + ", " + name2 + ", and " + name3 + " are all the same age")
+elif age1 > age2 and age2 > age3:
+    print(name1 + " at " + str(age1) + " is the oldest, whilst " + name3 + " at " + str(age3) + " is the youngest")
+elif age1 > age3 and age3 > age2:
+    print(name1 + " at " + str(age1) + " is the oldest, whilst " + name2 + " at " + str(age2) + " is the youngest")
+elif age2 > age1 and age1 > age3:
+    print(name2 + " at " + str(age2) + " is the oldest, whilst " + name3 + " at " + str(age3) + " is the youngest")
+elif age2 > age3 and age3 > age1:
+    print(name2 + " at " + str(age2) + " is the oldest, whilst " + name1 + " at " + str(age1) + " is the youngest")
+elif age3 > age1 and age1 > age2:
+    print(name3 + " at " + str(age3) + " is the oldest, whilst " + name2 + " at " + str(age2) + " is the youngest")
+elif age3 > age2 and age2 > age1:
+    print(name3 + " at " + str(age3) + " is the oldest, whilst " + name1 + " at " + str(age1) + " is the youngest")
 
 
 ## Question 7 ##
@@ -102,3 +121,47 @@ print("###Question 8###\n")
 # e. 25 to 45 – E
 # f. Below 25 - F
 # Ask user to enter the lesson and the marks for three lessons and print out the corresponding grades for the lesson.
+lessonname1 = input("What is Lesson #1? ")
+lessonmark1 = input("What mark did you get (0-100) for " + lessonname1 + "? ")
+lessonname2 = input("What is Lesson #2 ")
+lessonmark2 = input("What mark did you get (0-100) for " + lessonname2 + "? ")
+lessonname3 = input("What is Lesson #3? ")
+lessonmark3 = input("What mark did you get (0-100) for " + lessonname3 + "? ")
+if int(lessonmark1) < 25:
+    print("For " + lessonname1 + " your grade was - F" )
+elif int(lessonmark1) < 46:
+    print("For " + lessonname1 + " your grade was - E" )
+elif int(lessonmark1) < 51:
+    print("For " + lessonname1 + " your grade was - D" )
+elif int(lessonmark1) < 61:
+    print("For " + lessonname1 + " your grade was - C" )
+elif int(lessonmark1) < 81:
+    print("For " + lessonname1 + " your grade was - B" )
+elif int(lessonmark1) > 80:
+    print("For " + lessonname1 + " your grade was - A" )
+
+if int(lessonmark2) < 25:
+    print("For " + lessonname2 + " your grade was - F")
+elif int(lessonmark2) < 46:
+    print("For " + lessonname2 + " your grade was - E")
+elif int(lessonmark2) < 51:
+    print("For " + lessonname2 + " your grade was - D")
+elif int(lessonmark2) < 61:
+    print("For " + lessonname2 + " your grade was - C")
+elif int(lessonmark2) < 81:
+    print("For " + lessonname2 + " your grade was - B")
+elif int(lessonmark2) > 80:
+    print("For " + lessonname2 + " your grade was - A")
+
+if int(lessonmark3) < 25:
+    print("For " + lessonname3 + " your grade was - F")
+elif int(lessonmark3) < 46:
+    print("For " + lessonname3 + " your grade was - E")
+elif int(lessonmark3) < 51:
+    print("For " + lessonname3 + " your grade was - D")
+elif int(lessonmark3) < 61:
+    print("For " + lessonname3 + " your grade was - C")
+elif int(lessonmark3) < 81:
+    print("For " + lessonname3 + " your grade was - B")
+elif int(lessonmark3) > 80:
+    print("For " + lessonname3 + " your grade was - A")
