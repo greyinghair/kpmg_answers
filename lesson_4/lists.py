4c5f3eed6b6c2b202df6d34d7561fabba73d7523
greyinghair <dan@stacey.im>
2020-05-04 17:30:01 +0000

    lesson 4 section A

---
+++ lesson_4/lists.py
@@ -0,0 +1,52 @@
+## Lists are strings captured in [] separated by commas
+# e.g.
+names = ["Alice", "Bob", "Charlie"]
+# Print using indexing which represents each item starting at zero
+print(names[0]) # Alice
+print(names[1]) # Bob
+print(names[2]) # Charlie
+
+
+## To ADD to an existing list with 
+#
+#   <variable/list>.append
+#
+# e.g. add "Dave" to above 'names' variable
+names.append("Dave")
+print(names) # ['Alice, 'Bob', 'Charlie', 'Dave']
+
+
+## To REPLACE exsting item, define variable with the index position to replace
+# along with the new value.
+# e.g. to replace Charlie (which is index position 2) with Chris:
+names[2] = "Chris"
+print(names) # ['Alice', 'Bob', 'Chris', 'Dave']
+
+
+## To DELETE an item in a list, define variable with DEL and the index position 
+# e.g. to delete 'Bob' (which is index position 1 (2nd from the left)):
+del(names[1])
+print(names) # ['Alice', 'Chris', 'Dave']
+
+
+## To check presence of an item in a list use IN within the statement
+# e.g. to see if "Eve" is in the list
+names = ["Alice", "Bob", "Charlie"]
+if "Eve" in names:
+  print("Eve is here")
+else:
+  print("Eve is NOT here")
+
+
+## To see the LENGTH of a list, ie the number of items in a list use the LEN statement
+print(len(names)) # 3 items in the list
+
+
+## SORT list 
+#
+#   alphabetically <list>.sort()
+#
+# e.g.
+names = ["Charlie", "Alice", "Bob"]
+names.sort()
+print(names) # Should be ['Alice', 'Bob', 'Charlie']
---
+++ lesson_4/questions_b.py
@@ -0,0 +1,42 @@
+# 1 .Loop through the list you created in section A and print each item out
+
+
+
+# 2. Print the numbers 1 to 100 (including the number 100)
+
+
+
+# 3. Print all odd numbers from 1 to 100
+
+
+
+# 4. The modern olympics started in 1896, print the years they have been held (bonus points to skip 1916, 1940, 1944)
+
+
+
+# 5. Create a list of ten random numbers. Loop through your list and count the number of even numbers and the number of odd numbers.
+
+
+
+
+# 6. Create a list of five names. Write a loop that will print "Hello" plus each name in the list.
+
+
+
+
+# 7. Create a loop to go through each letter of the word "supercalifragilisticexpialidocious".
+
+
+
+# 8. Create a list of 5 numbers. Write a for loop which appends the square of each number to the new list.
+
+
+
+
+# 9. Create a list with five names in. Write a for loop which appends Dr. to each name in the new list.
+
+
+
+
+
+# 10. FizzBuzz – Write a program that prints the numbers from 1 to 100. For multiples of three, print "Fizz" instead of the number and for multiples of five, print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz"
