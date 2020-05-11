c4e98965199bd8e4d6e7d223ba5be42943f2a1dc
Dan Stacey <dan@stacey.im>
2020-05-11 13:25:23 +0100

    Lesson_5 (Q1 - Q3)

--- lesson_5/Questions/q1.py
+++ lesson_5/Questions/q1.py
@@ -1,1 +1,4 @@
 # Print 10 random numbers
+import random
+for x in range(10): # loop 10 times
+    print(random.random()) # Prints a random float
--- lesson_5/Questions/q2.py
+++ lesson_5/Questions/q2.py
@@ -1,1 +1,6 @@
 # Keep asking the user to enter a number until they enter the number 7, then print "Wow lucky number 7!"
+number = 0
+while number != 7: # As long as not 7 then keep looping (7 is INT hence no quotes)
+    number = int(input("Pick a number..")) # MUST cast to integer or else is str and while statement is never False
+    print("You chose " + str(number))
+print("Wow lucky number 7!")
