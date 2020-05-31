51c5386fa9d6161370d9dfdb2eef76c1607cba4f
Dan Stacey <dan@stacey.im>
2020-05-31 19:21:07 +0100

    lesson_7 - Question Section B - question 6 (working)

--- lesson_7 (functions)/Questions/section_b.py
+++ lesson_7 (functions)/Questions/section_b.py
@@ -61,18 +61,23 @@ padlock(int(input("What is the passcode? ")))
 # Write a function that returns the sum of multiples of 3 and 5 between 0 and limit (parameter). For example,
 # if limit is 20, it should return the sum of 3, 5, 6, 9, 10, 12, 15, 18, 20
 
+
+
 # Q6
 # Write a function called is_prime() that accepts a number and return True or False if the number of prime or not
 
 def is_prime(prime):
     count = 0
-    for p in range(1,(prime)):
-        if prime % p > 1:
+    for p in range(2,(prime)): # loop from 2 through to index 1 less than the number itself, i.e. won't include the
+        # number itself nor 1 but divide by everything else, if any of then can be divided by a whole number then not
+        # a Prime number
+        if prime % p == 0: # If the number is divisible (entirely by the number in range, then:
+            print("Modulus of " + str(prime) + " % " + str(p) + " is " + str((prime) % p))
             count += 1
     if count <= 2:
-        print("PRIME" + " (count: " + str(count))
+        print("PRIME" + " (count: " + str(count) + ")")
     else:
-        print("NOT PRIME" + " (count: " + str(count))
+        print("NOT PRIME" + " (count: " + str(count) + ")")
 
 
 is_prime(int(input("What number would you like to know whether is a prime number or not? ")))
