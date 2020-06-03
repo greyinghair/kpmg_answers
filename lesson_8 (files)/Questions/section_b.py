d5429f146e3bfccff43fe754d5f8077d8357fb13
greyinghair <58940142+greyinghair@users.noreply.github.com>
2020-06-03 17:54:42 +0100

    lesson_8 - Questions Section B - Q3 answered (KMPG version)

--- lesson_8 (files)/Questions/section_b.py
+++ lesson_8 (files)/Questions/section_b.py
@@ -81,9 +81,53 @@ for num in secret:
 
 secret.close()
 
+# Q3 - kpmg way
+# use a dictionary and then the numbers equate to the index position in said dict.
+
+alphabet = {
+    1: "A",
+    2: "B",
+    3: "C",
+    4: "D",
+    5: "E",
+    6: "F",
+    7: "G",
+    8: "H",
+    9: "I",
+    10: "J",
+    11: "K",
+    12: "L",
+    13: "M",
+    14: "N",
+    15: "O",
+    16: "P",
+    17: "Q",
+    18: "R",
+    19: "S",
+    20: "T",
+    21: "U",
+    22: "V",
+    23: "W",
+    24: "X",
+    25: "Y",
+    26: "Z"
+}
+
+word = "" # empty variable str so can add each letter to it
+for num in open("secret.txt", "r"):
+    x = int(num)
+    word += alphabet[x]
+print(word)
+
 # Q4.
 # Benford’s law states that the leading digits in a collection of data are probably going to be small.
 # For example, most numbers in a set (about 30%) will have a leading digit of 1, when the expected
 # probability is 11.1% (i.e. one out of nine digits). Fake data is usually evenly distributed, where as
 # real data The files 'accounts_1.txt', 'accounts_2.txt' and 'accounts_3.txt' contain financial transaction data.
 # Work out which of the files contains fake data.
+
+    # random generate numbers x 10000 to act as baseline, print to screen % found of each one 1 to 9.
+    # get length of each file (def)
+    # what percentage entries start with a 1 in each file, or 2 or 3 etc.
+    # FYI. most evenly spreadh percentage will be fake file
+    # get script to query each accounts file based on account_ +*
