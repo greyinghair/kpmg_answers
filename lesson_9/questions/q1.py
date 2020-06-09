84a0f93b6ef08c322b8a74de466095f4b20bdeff
Dan Stacey <dan@stacey.im>
2020-06-09 18:06:34 +0100

    lesson_9 - Questions - Q1 done

--- lesson_9/questions/q1.py
+++ lesson_9/questions/q1.py
@@ -1,2 +1,13 @@
 # Q1
 # Create a function that returns the first and last letters of the word passed in as separate variables.
+
+def first_last(*args):
+    for x in args: # loop each word
+        for y in x: # loop each letter in word
+            print(y[0],y[-1]) # print index 0 and index -1 (ie 1st & last)
+
+# List to add as many strings to as required
+words = ["daniel", "stacey"]
+
+# Call function and pass the list 'words'
+first_last(words)
