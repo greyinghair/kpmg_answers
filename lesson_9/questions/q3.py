564693308d3feafe6a5cb311875118005a9d2bce
Dan Stacey <dan@stacey.im>
2020-06-09 20:18:56 +0100

    lesson_9 - Questions - Q3 done

--- lesson_9/questions/q3.py
+++ lesson_9/questions/q3.py
@@ -1,2 +1,29 @@
 # Q3
-# Create a function that takes a file name and word. Search the file to find all lines that start with the given word. Test your function using the file 'austen.txt' and find all lines that begin with "Mr". Hint: is a function that returns True/False if a string ends with a value...maybe there is one that checks to see if a string "starts with"?
+# Create a function that takes a file name and word. Search the file to find all lines that start with the given word.
+#
+# Test your function using the file 'austen.txt' and find all lines that begin with "Mr".
+# Hint: is a function that returns True/False if a string ends with a value...
+#   maybe there is one that checks to see if a string "starts with"?
+
+
+# should be 4.  1 of the lines starts "Mr. instead of Mr. so need to detect remove the starting "
+
+filename = open("files/austen.txt","r")
+word_to_find = "Mr"
+
+def find(word, file):
+    count = 0
+    ignore = ["\""] # Have to escape the quotation "
+    for x in ignore: # loop for each entry in ignore list
+        for line in file: # loop for each line in the file
+            if line.startswith(word) or line.startswith(x + word):
+                print(line) # show the line found to match on each loop
+                count += 1 # Increment count on each detection
+
+    file.close()
+
+    return count # return value of count post loop
+
+
+
+print(find(word_to_find,filename))
