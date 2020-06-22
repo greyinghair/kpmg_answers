dc1e13a988c14aebd0caaa839a2e0418d8a04c02
Dan Stacey <dan@stacey.im>
2020-06-22 17:25:36 +0100

    lesson_10 - Q1 working

--- lesson_10 (OOP)/question_a/question_1.py
+++ lesson_10 (OOP)/question_a/question_1.py
@@ -1,3 +1,12 @@
 ## Q1
 # Create a Person class, initialise it with a name.
 # Create a method for the Person class that will say hello to the name.
+
+class Person():
+    name = "Dan"
+
+    def name_func(self,name):
+        print("Hello " + self.name)
+
+
+print(Person.name)
